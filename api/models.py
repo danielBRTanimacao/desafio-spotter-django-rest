8725c264c26e51cc33b0de85b158c30f712221d5
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-09-18 10:55:42 -0300

    build: creating principal models Author, Book, FavoriteBook

--- api/models.py
+++ api/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-class Book(): ...
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
 
-class Author(): ...
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
 
-class User(): ...
+class Book(models.Model):
+    author = models.OneToOneField(Author, on_delete=models.CASCADE, unique=True)
+    name = models.CharField(max_length=255)
+
+class FavoriteUserBook(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    add_date = models.DateTimeField(default=timezone.now)
