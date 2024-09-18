e177e38954bc34b8d05ccccd64fa5ac622596109
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-09-18 11:16:39 -0300

    build: created models and configured admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from api.models import Author, Book, FavoriteUserBook
 
 # Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'first_name',
+        'last_name',
+    )
+    ordering = '-id',
+    search_fields = (
+        'id',
+        'first_name',
+        'last_name',
+    )
+    list_per_page = 10
+    list_max_show_all = 150
+    list_display_links = 'id', 'first_name',
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = 'id', 'author', 'name',
+    ordering = '-id',
+    search_fields = (
+        'id',
+        'author',
+        'name',
+    )
+    list_per_page = 10
+    list_max_show_all = 150
+    list_display_links = 'id', 'author',
+
+@admin.register(FavoriteUserBook)
+class FavoriteUserBookAdmin(admin.ModelAdmin):
+    list_display = 'id', 'user', 'book',
+    ordering = '-id',
+    search_fields = (
+        'id',
+        'user',
+        'book',
+    )
+    list_per_page = 10
+    list_max_show_all = 150
+    list_display_links = 'id', 'user',
--- api/models.py
+++ api/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
@@ -10,10 +12,22 @@ class Author(models.Model):
         return f'{self.first_name} {self.last_name}'
 
 class Book(models.Model):
-    author = models.OneToOneField(Author, on_delete=models.CASCADE, unique=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f'author: {self.author.first_name} book name: {self.name}'
 
 class FavoriteUserBook(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, blank=True, null=True)
     add_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self) -> str:
+        return f'{self.user.username}'
+
+
+@receiver(post_save, sender=User)
+def add_favorite_user_book(sender, instance, created, **kwargs):
+    if created:
+        FavoriteUserBook.objects.create(user=instance)
