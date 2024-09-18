546f9f22d43980383507c507c6e7a92e5d120d53
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-09-18 17:08:26 -0300

    feat: simple rename variables

--- api/views.py
+++ api/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Author, Book, FavoriteUserBook
-from .serializers import AuthorSerializer, BookSerializer, FavoriteUserBookSerializer, UserSerializer
+from .serializers import AuthorSerializer, BookSerializer, UserSerializer
 
 class BookApiView(APIView):
     permission_classes = [AllowAny]
@@ -19,7 +19,7 @@ class BookApiView(APIView):
     def get(self, request):
         books = Book.objects.all()
         serializer = BookSerializer(books, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
         serializer = BookSerializer(data=request.data)
@@ -39,14 +39,14 @@ class BookDetailApiView(APIView):
     def get(self, request, id):
         book = get_object_or_404(Book, pk=id)
         serializer = BookSerializer(book)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     def put(self, request, id):
         book = get_object_or_404(Book, pk=id)
         serializer = BookSerializer(book, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     
     def delete(self, request, id):
         book = get_object_or_404(Book, pk=id)
@@ -66,12 +66,13 @@ class AuthorApiView(APIView):
     def get(self, request):
         authors = Author.objects.all()
         serializer = AuthorSerializer(authors, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     def post(self, request):
         serializer = AuthorSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     
 
 class AuthorDetailApiView(APIView):
@@ -85,14 +86,14 @@ class AuthorDetailApiView(APIView):
     def get(self, request, id):
         author = get_object_or_404(Book, pk=id)
         serializer = BookSerializer(author)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, id):
         author = get_object_or_404(Author, pk=id)
         serializer = BookSerializer(author, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     
     def delete(self, request, id):
         author = get_object_or_404(Author, pk=id)
@@ -105,33 +106,20 @@ class UserRegisterApiView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-
-        if not username or not password:
-            return Response({"error": "All fields are need"}, status=status.HTTP_400_BAD_REQUEST)
-        if User.objects.filter(username=username).exists():
-            return Response({"error": "User exist"}, status=status.HTTP_400_BAD_REQUEST)
-        
-        user = User.objects.create(username=username, password=password)
-        serializer = UserSerializer(user)
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        authenticate(serializer)
+        # serializer.save() {"username": "pessoa", "password":"12345678i"}
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class UserLoginApiView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-
-        user = authenticate(username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            serializer = UserSerializer(user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response({"error": "Invalid fields"}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        login(serializer, User)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class UserApiView(APIView):
@@ -140,8 +128,4 @@ class UserApiView(APIView):
     def get(self, request):
         user = request.user
         serializer = UserSerializer(user)
-        return Response(serializer.data)
-
-    # add book
-
-    # remove book
+        return Response(serializer.data, status=status.HTTP_200_OK)
--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -88,17 +88,15 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
 }
 
-# SIMPLE_JWT = {
-#     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-#     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-#     'ROTATE_REFRESH_TOKENS': True,
-#     'BLACKLIST_AFTER_ROTATION': True,
-# }
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+}
